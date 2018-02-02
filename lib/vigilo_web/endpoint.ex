e921e4bd0423616ff8225e7597a1baef449b0934
Michael Davis <mcd164@miami.edu>
2018-02-01 23:26:41 -0500

    Add an eye for apple touch icon
    For making the website an "app" on an apple homescreen, you can have a
    png file. I've chosen a random eye from google images search. If you
    intend to use this project for any serious means, you should probably
    change that

--- lib/vigilo_web/endpoint.ex
+++ lib/vigilo_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule VigiloWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :vigilo, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt apple-touch-icon-120x120.png)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
